042c4c5cc26951a98b07dfd6ea9057f327812bed
Will Hodgson <will.hodgson@current-rms.com>
2019-11-22 13:22:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,4 +16,4 @@ var doubler = createMultiplier(2);
 var tripler = createMultiplier(3);
 
 var doublerWithBind = multiplier.bind(null, 2);
-var triplerWithBind = multiplier.bind(null, 3)
+var triplerWithBind = multiplier.bind(null, 3);
