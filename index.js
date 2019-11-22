7afd862a517deb58cef7ca654c822ab9f0f54ad0
Will Hodgson <will.hodgson@current-rms.com>
2019-11-22 13:18:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ function createMultiplier(multiplierValue) {
   }
 }
 
-var doubler = createMultiplier(2);
-var tripler = createMultiplier(3);
 
 function multiplier(a, b) {
   return a * b
 }
+
+var doubler = createMultiplier(2);
+var tripler = createMultiplier(3);
